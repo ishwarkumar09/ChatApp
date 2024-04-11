eab4f002794002617e6ae0a498c009f0a14a72cc
ishwarkumar09 <ishwarborana4600@gmail.com>
2024-04-11 22:19:18 +0530

    add getUserForSidebar controller

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./db/index.js";
 
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from"./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js"
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -21,6 +22,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+app.use("/api/user",userRoutes )
 
 connectDB()
 .then(()=>{
