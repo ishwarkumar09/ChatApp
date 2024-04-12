2971bd1cc6c135657bafb55cec3b3fb0997bce31
ishwarkumar09 <ishwarborana4600@gmail.com>
2024-04-12 11:58:53 +0530

    frontend files

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,16 @@
+function Login(){
+
+
+
+return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto "> 
+         <div className="w-full p-6 rounded-1g shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-1g bg-opacity-0">
+            <h1 className="text-3x1 font-semibold text-center text-gray-300">
+               Login
+            </h1>
+         </div>
+    </div>
+)
+}
+
+export default Login;
