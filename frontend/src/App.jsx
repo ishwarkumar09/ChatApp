646afbf1c1c83cc17059e7ab1f403fc4bbd957af
ishwarkumar09 <ishwarborana4600@gmail.com>
2024-04-13 13:09:09 +0530

    add features in messageContainer and  create MessageInput.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,8 +9,8 @@ function App() {
   return (
   <>
    <div className='p-4 h-screen flex items-center justify-center'>
-   <SignUp></SignUp>
-   {/* <Home></Home> */}
+   {/* <SignUp></SignUp> */}
+   <Home></Home>
    </div>
 
   </>
