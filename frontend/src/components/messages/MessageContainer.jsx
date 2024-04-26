82bb957a7a92659d8f937a55a6b7f56c987d5b32
ishwarkumar09 <ishwarborana4600@gmail.com>
2024-04-26 10:01:04 +0530

    create useConversation useGetConversation

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,17 +1,24 @@
 import Messages from "./Messages.jsx"
 import MessageInput from "./MessageInput.jsx"
 import {TiMessages} from "react-icons/ti"
+import useConversation from "../../zustand/useConversation.js"
+import { useEffect } from "react";
 
 
 function MessageContainer (){
-    const noChatSelected = true;
+    const{selectedConversation ,setSelectedConversation} = useConversation();
+
+    useEffect(()=>{
+      //cleanup function (unmounts)
+      return ()=>setSelectedConversation(null)
+    },[setSelectedConversation])
 
     return (
         <div className="md:min-w-[450px] flex flex-col">
-        {noChatSelected ? <NoChatSelected /> : (
+        {!selectedConversation ? <NoChatSelected /> : (
         <> {/* Header */}
            <div className="bg-slate-500 px-4 py-2 mb-2">
-            <span className="label-text">To:</span> <span className="text-gray-900 font-bold">Ishu</span>
+            <span className="label-text">To:</span> <span className="text-gray-900 font-bold">{selectedConversation.fullName}</span>
            </div>
             <Messages />
            <MessageInput />
