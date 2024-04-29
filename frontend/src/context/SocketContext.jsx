759bd270ecfded8c1ff80fc495493fb4da83c889
ishwarkumar09 <ishwarborana4600@gmail.com>
2024-04-29 13:51:24 +0530

    debug url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chatapp-ogam.onrender.com/",{
+      const socket = io("https://chatapp-ogam.onrender.com",{
         query:{
           userId :authUser._id,
         }
